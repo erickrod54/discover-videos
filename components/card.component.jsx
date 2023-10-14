777ba4e99f1092a027b7b330e2ad0822f0eea5d2
Erick Rodriguez <erickrod54@gmail.com>
2023-10-14 16:41:18 -0400

    Discover-video-app - version 1.03 - Card js - Features: Building basic Card component

---
+++ components/card.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**Discover-videos-app - version 1.03  - Card js - Features:
+ * 
+ *      --> Building basic 'Card' component.
+ * 
+ * Note: This is part of the initial composition of the app,
+ * after this step every component will developed indiviually
+*/
+
+const Card = () => {
+
+    return(
+        <h2>Card Component</h2>
+    )
+}
+
+export default Card;

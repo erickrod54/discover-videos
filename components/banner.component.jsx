419a469ba8a653821d7ad9e9d5dc19498d5072be
Erick Rodriguez <erickrod54@gmail.com>
2023-10-14 16:40:02 -0400

    Discover-videos-app - version 1.03 - Banner js - Features: Building basic banner component

---
+++ components/banner.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**Discover-videos-app - version 1.03  - Banner js - Features:
+ * 
+ *      --> Building basic 'banner' component.
+ * 
+ * Note: This is part of the initial composition of the app,
+ * after this step every component will developed indiviually
+*/
+
+const Banner = () => {
+
+    return(
+        <h2>Banner Component</h2>
+    )
+}
+
+export default Banner;

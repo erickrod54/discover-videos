5832a0b5f289f821f2a91e2c7870f5d20fe65cf7
Erick Rodriguez <erickrod54@gmail.com>
2023-10-14 16:42:41 -0400

    Discover-videos-app - version 1.03 - Navbar js - Features: Building basic Navbar component

---
+++ components/navbar.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**Discover-videos-app - version 1.03  - Navbar js - Features:
+ * 
+ *      --> Building basic 'Navbar' component.
+ * 
+ * Note: This is part of the initial composition of the app,
+ * after this step every component will developed indiviually
+*/
+
+const Navbar = () => {
+
+    return(
+        <h2>Navbar Component</h2>
+    )
+}
+
+export default Navbar;

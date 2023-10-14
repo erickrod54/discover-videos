3daa7250ba1a37bdac5540d6500edfdee6fbe4cd
Erick Rodriguez <erickrod54@gmail.com>
2023-10-14 16:44:07 -0400

    Discover-videos-app - version 1.03 - components > index js - Features: Adding components to the index

---
+++ components/index.js
@@ -0,0 +1,13 @@
+import Banner from "./banner.component";
+import Card from "./card.component";
+import Navbar from "./navbar.component";
+
+/**Discover-videos-app - version 1.03  - components > index js - Features:
+ * 
+ *      --> Adding components to the index.
+ * 
+ * Note: This is part of the initial composition of the app,
+ * after this step every component will developed indiviually
+*/
+
+export { Navbar, Banner, Card }

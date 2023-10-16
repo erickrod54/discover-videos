c1ed2b1cc7fdefa677f781da01d8c7ec7697215c
Erick Rodriguez <erickrod54@gmail.com>
2023-10-15 21:30:00 -0400

    Discover-videos-app - version 1.04 - Banner js - Features: Work in progress building banner

--- components/banner.component.jsx
+++ components/banner.component.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 
-/**Discover-videos-app - version 1.03  - Banner js - Features:
+
+/**Discover-videos-app - version 1.04  - Banner js - Features:
  * 
- *      --> Building basic 'banner' component.
+ *      --> Work in progress building banner.
  * 
- * Note: This is part of the initial composition of the app,
- * after this step every component will developed indiviually
+ * Note: so fas props has been passed as placehodlers 
+ * from the main component in the index js
 */
 
-const Banner = () => {
+const Banner = (props) => {
 
+    const {  title, subtitle, imgUrl } = props;
+
+   
+    
     return(
-        <h2>Banner Component</h2>
+        <>
+            <div>
+                <h2>
+                    {title}
+                </h2>
+                <h3>
+                    {subtitle}
+                </h3>
+            </div>
+            <div
+                style={{
+                backgroundImage: `url(${imgUrl})`,
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                backgroundSize: "cover",
+                backgroundPosition: "50% 50%",
+                }}
+            ></div>
+        </>
+        
     )
 }
 

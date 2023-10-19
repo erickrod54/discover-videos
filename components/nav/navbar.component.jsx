758e59e6d4c61a629baf7fc96b7705b34fdf88a8
Erick Rodriguez <erickrod54@gmail.com>
2023-10-18 23:53:00 -0400

    Discover-videos-app - version 1.11 - Navbar js - Features: Writting classNames

--- components/nav/navbar.component.jsx
+++ components/nav/navbar.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./navbar.module.css"
 
-/**Discover-videos-app - version 1.10  - Navbar js - Features:
+/**Discover-videos-app - version 1.11  - Navbar js - Features:
  * 
- *      --> Writting the complete jsx structure.
+ *      --> Writting classNames .
  * 
  * Note: This is part of the initial composition of the app,
  * after this step every component will developed indiviually
@@ -13,20 +14,29 @@ const Navbar = (props) => {
     const { username } = props;
 
     return(
-        <div>
-            Navbar Component
-            <p>{username}</p>
-            <ul>
-                <li>Home</li>
-                <li>My List</li>
+        <div className={styles.container}> 
+            <div className={styles.wrapper}>
+                <a className={styles.logoLink} href="/">
+                    <div className={styles.logoWrapper}>
+                    N Series
+                    </div>
+                </a>
+            </div>
+            <ul className={styles.navItems}>
+                <li className={styles.navItem}>Home</li>
+                <li className={styles.navItem2}>My List</li>
             </ul>
-            <nav>
+            <nav className={styles.navContainer}>
                 <div>
-                    <button>
-                        <p>{username}</p>
+                    <button className={styles.usernameBtn}>
+                        <p className={styles.username}>{username}</p>
+                    {/**expand more icon */}
                     </button>
-                    <div>
-                        Sign Out
+                    <div className={styles.navDropdown}>
+                        <a className={styles.linkName}> 
+                            Sign Out
+                        </a>
+                        <div className={styles.lineWrapper}></div>
                     </div>
                 </div>
             </nav>

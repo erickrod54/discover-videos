77a7ed011b4dd1b7b7c4a8cf05daca50cba9e23e
Erick Rodriguez <erickrod54@gmail.com>
2023-10-24 21:30:00 -0400

    Discover-videos-app - version 2.05 - Card js - Fetaures: Adding images domain unplash

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+/**Discover-videos-app - version 2.05  - Card js - Features:
+ * 
+ *      --> Adding 'images' domain unplash. 
+ * 
+ * Note: this is made in order to get the image
+ * rendered
+*/
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images:{
+    domains:['images.unsplash.com']
+  }
 }
 
 module.exports = nextConfig

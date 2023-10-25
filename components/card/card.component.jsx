bc306f4351929333d08bd78c600fa235ed172a98
Erick Rodriguez <erickrod54@gmail.com>
2023-10-24 21:30:00 -0400

    Discover-videos-app - version 2.04 - Card js - Features: Handling Image Error using imgSrc

--- components/card/card.component.jsx
+++ components/card/card.component.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import styles from './card.module.css'
 
-/**Discover-videos-app - version 2.03  - Card js - Features:
+/**Discover-videos-app - version 2.04  - Card js - Features:
  * 
- *      --> Handling Image Error. 
+ *      --> Handling Image Error using 'imgSrc'. 
  * 
  * Note: In order to add the pointer and more styles to the 
  * container
@@ -16,6 +16,8 @@ const Card = (props) => {
 
     const { imgUrl = '/statics/clifford.the.red.dog.png', size = 'medium' } = props;
 
+    const [ imgSrc, setImgSrc ] = useState(imgUrl)
+
     const classMap = {
         large: styles.lgItem,
         medium: styles.mdItem, 
@@ -24,6 +26,7 @@ const Card = (props) => {
 
     const handleOnError = () => {
         console.log('hii error')
+        setImgSrc('/statics/clifford.the.red.dog.png')
     }
 
 
@@ -31,7 +34,7 @@ const Card = (props) => {
         <div className={styles.container}>
             <div className={classMap[size]} >
                 <Image  
-                    src={imgUrl} 
+                    src={imgSrc} 
                     alt="image" 
                     layout="fill" 
                     onError={handleOnError}

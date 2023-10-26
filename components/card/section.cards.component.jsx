13084e618d45f52e3ff8a0b5295269bce11c71b9
Erick Rodriguez <erickrod54@gmail.com>
2023-10-26 13:47:23 -0400

    Discover-videos-app - version 2.07 - SectionsCards - Features: Building Basic SectionCards

---
+++ components/card/section.cards.component.jsx
@@ -0,0 +1,15 @@
+/**Discover-videos-app - version 2.07 - SectionCards- Features:
+ * 
+ *      --> Building Basic 'SectionCards'
+ * 
+ * Note: This component will wrap the cards and make them scroll
+*/
+
+const SectionCards = () => {
+
+    return(
+        <div>SectionCards</div>
+    )
+}
+
+export default SectionCards

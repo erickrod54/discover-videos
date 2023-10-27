b28058922e2f6a1e597d0cd9a48e77a9a1e66316
Erick Rodriguez <erickrod54@gmail.com>
2023-10-27 03:00:40 -0400

    Discover-videos-app - version 2.09 - SectionCards - Features: Adding Card size large to the section

--- components/card/section.cards.component.jsx
+++ components/card/section.cards.component.jsx
@@ -1,9 +1,9 @@
 import Card from "./card.component";
 import styles from './section.cards.module.css';
 
-/**Discover-videos-app - version 2.08 - SectionCards- Features:
+/**Discover-videos-app - version 2.09 - SectionCards- Features:
  * 
- *      --> Adding classNames modules for styles.
+ *      --> Adding 'Card' large section.
  * 
  * Note: This component will wrap the cards and make them scroll
 */
@@ -17,7 +17,7 @@ const SectionCards = (props) => {
         <section className={styles.container}>
             <h2 className={styles.title}>Disney</h2>
             <div className={styles.cardWrapper}>
-                 <Card imgUrl='/statics/clifford.the.red.dog.png' size='large'/>
+                <Card imgUrl='/statics/clifford.the.red.dog.png' size='large'/>
             </div>
         </section>
     )

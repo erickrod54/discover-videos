eb136cc325b1fcb9db9b7f6ba8f8fee631e42748
Erick Rodriguez <erickrod54@gmail.com>
2023-10-27 02:48:44 -0400

    Discover-videos-app - version 2.08 - SectionCards - Features: Adding classNames modules for styles

--- components/card/section.cards.component.jsx
+++ components/card/section.cards.component.jsx
@@ -1,14 +1,25 @@
-/**Discover-videos-app - version 2.07 - SectionCards- Features:
+import Card from "./card.component";
+import styles from './section.cards.module.css';
+
+/**Discover-videos-app - version 2.08 - SectionCards- Features:
  * 
- *      --> Building Basic 'SectionCards'
+ *      --> Adding classNames modules for styles.
  * 
  * Note: This component will wrap the cards and make them scroll
 */
 
-const SectionCards = () => {
+
+const SectionCards = (props) => {
+
+    const { title } = props;
 
     return(
-        <div>SectionCards</div>
+        <section className={styles.container}>
+            <h2 className={styles.title}>Disney</h2>
+            <div className={styles.cardWrapper}>
+                 <Card imgUrl='/statics/clifford.the.red.dog.png' size='large'/>
+            </div>
+        </section>
     )
 }
 

899b9cfdc78b70c1089cabe5c8b3700dd5710efd
Erick Rodriguez <erickrod54@gmail.com>
2023-10-29 02:30:28 -0400

    Discover-videos-app - version 2.11 - SectionCards - Features: Adding an id prop to test the scale flow

--- components/card/section.cards.component.jsx
+++ components/card/section.cards.component.jsx
@@ -1,9 +1,9 @@
 import Card from "./card.component";
 import styles from './section.cards.module.css';
 
-/**Discover-videos-app - version 2.10 - SectionCards- Features:
+/**Discover-videos-app - version 2.11 - SectionCards- Features:
  * 
- *      --> Adding 'Card' component to visualize the scroll effect.
+ *      --> Adding an 'id' prop to test the scale flow.
  * 
  * Note: This scroll currentlly works in the inspection browser window
  * and for full screen i have to tap in the mouse wheel
@@ -18,7 +18,7 @@ const SectionCards = (props) => {
         <section className={styles.container}>
             <h2 className={styles.title}>{title}</h2>
             <div className={styles.cardWrapper}>
-                <Card imgUrl='/statics/clifford.the.red.dog.png' size='large'/>
+                <Card id={0} imgUrl='/statics/clifford.the.red.dog.png' size='large'/>
                 <Card imgUrl='/statics/clifford.the.red.dog.png' size='large'/>
                 <Card imgUrl='/statics/clifford.the.red.dog.png' size='large'/>
                 <Card imgUrl='/statics/clifford.the.red.dog.png' size='large'/>

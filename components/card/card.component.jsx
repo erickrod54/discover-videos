72c29c38c88b64d0dfadf1eba43ac2f9f502e068
Erick Rodriguez <erickrod54@gmail.com>
2023-10-29 02:29:25 -0400

    Discover-videos-app - version 2.11 - Card js - Features: Writting scale flow to switch from 1.1 to 1.2, Destructuring id from props

--- components/card/card.component.jsx
+++ components/card/card.component.jsx
@@ -4,9 +4,11 @@ import styles from './card.module.css';
 import { motion } from "framer-motion";
 import cls from 'classnames';
 
-/**Discover-videos-app - version 2.10  - Card js - Features:
+/**Discover-videos-app - version 2.11  - Card js - Features:
  * 
- *      --> Changin the scale from the 'Card' to the Y axis.
+ *      --> Writting and 'scale' flow to switch from 1.1 to 1.2.
+ * 
+ *      --> Destructuring 'id' from the props
  * 
  * Note: 'classnames' has a 'shortname convention' of 'cls' 
  * to make the cascade styles behavior.
@@ -14,7 +16,7 @@ import cls from 'classnames';
 
 const Card = (props) => {
 
-    const { imgUrl = 'https://images.unsplash.com/photo-1485846234645-a62644f84728?auto=format&fit=crop&q=80&w=2059&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', size = 'medium' } = props;
+    const { imgUrl = 'https://images.unsplash.com/photo-1485846234645-a62644f84728?auto=format&fit=crop&q=80&w=2059&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', size = 'medium', id } = props;
 
     const [ imgSrc, setImgSrc ] = useState(imgUrl)
 
@@ -29,12 +31,13 @@ const Card = (props) => {
         setImgSrc('https://images.unsplash.com/photo-1485846234645-a62644f84728?auto=format&fit=crop&q=80&w=2059&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
     }
 
+    const scale = id === 0 ? { scale: 1.1 } : { scaleY: 1.2 }
 
     return(
         <div className={styles.container}>
             <motion.div 
                 className={cls(styles.imgMotionWrapper, classMap[size])} 
-                whileHover={{ scaleY: 1.2 }}>
+                whileHover={{...scale}}>
                 <Image  
                     src={imgSrc} 
                     alt="image" 

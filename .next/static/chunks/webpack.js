67252714558748c95a3cd902f22e34b33cdcdd94
Erick Rodriguez <erickrod54@gmail.com>
2023-11-03 21:30:00 -0400

    Sewrver Files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "839a291591a47b5c"; }
+/******/ 		__webpack_require__.h = function() { return "798a429f0935d1d7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

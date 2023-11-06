f45dd6586cbdd844a4a184339e7189c81af33333
Erick Rodriguez <erickrod54@gmail.com>
2023-11-06 01:21:33 -0500

    Discover-videos-app - version 3.02 - login js - Features: IOmporting and implementing HEAD to set a title to the tab, displaying the login message

---
+++ pages/login.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+
+/**Discover-videos-app - version 3.02 - login js - Features:
+ * 
+ *      --> Importing and implementing 'HEAD' toi set a title
+ *          to the tab
+ * 
+ *      --> displaying the 'login' message
+ * 
+ * Note: by creating the login js i ca start to write and build 
+ * the Login page
+*/
+
+const Login = () => {
+    return(
+        <div>
+        <Head>
+            <title>Netseries Sign In</title>
+        </Head>
+
+        <div>
+            Login
+        </div>
+        </div>
+    )
+}
+
+export default Login;

caf20dafda369feb8bac1ef4b6782e86d307059f
Erick Rodriguez <erickrod54@gmail.com>
2023-11-06 01:44:32 -0500

    Discover-videos-app - version 3.03 - login js - Features: getting the logo and starting to style

--- pages/login.js
+++ pages/login.js
@@ -1,26 +1,39 @@
 import Head from "next/head";
+import Link from "next/link";
+import styles from '../styles/login.module.css'
+import Image from "next/image";
 
-/**Discover-videos-app - version 3.02 - login js - Features:
+/**Discover-videos-app - version 3.03 - login js - Features:
  * 
- *      --> Importing and implementing 'HEAD' toi set a title
- *          to the tab
- * 
- *      --> displaying the 'login' message
+ *      --> getting the logo and starting to style
  * 
  * Note: by creating the login js i ca start to write and build 
  * the Login page
 */
 
 const Login = () => {
-    return(
+    return (
         <div>
-        <Head>
-            <title>Netseries Sign In</title>
-        </Head>
+            <Head>
+                <title>Netseries Sign In</title>
+            </Head>
 
-        <div>
-            Login
-        </div>
+            <header>
+                <div className={styles.headerWrapper}>
+                    <Link href="/"> {/* Note that I removed the <a> tag */}
+                        <div className={styles.logoLink}>
+                            <div className={styles.logoWrapper}>
+                                <Image
+                                    src={'/statics/n.series1.svg'} 
+                                    alt="Netseries logo"
+                                    width={128}
+                                    height={34}
+                                />
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            </header>
         </div>
     )
 }

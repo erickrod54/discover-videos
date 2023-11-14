b630a95e81b140d920fbd47433ba32903c3d6a15
Erick Rodriguez <erickrod54@gmail.com>
2023-11-13 21:30:00 -0500

    Discover-videos-app - version 4.00 - pafges > _app.js - Features: Implementing useEffect to display login page as default

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { magic } from "../lib/magic-client";
+import "../styles/globals.css";
+
+/**Discover-videos-app - version 4.00  - pages > _app.js - 
+ * Features:
+ * 
+ *      --> Implementing 'useEffect' to display login
+ *          page as default 
+ * 
+ * Note: this implementation also allows to secure access 
+ * omnly to an authenticated user
+*/
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+      if (isLoggedIn) {
+        // route to /
+        router.push("/");
+      } else {
+        // route to /login
+        router.push("/login");
+      }
+    };
+    handleLoggedIn();
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

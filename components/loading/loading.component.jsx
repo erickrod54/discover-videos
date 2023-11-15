0a735b3fa0ef1cc096d721fff82c27b135d81b59
Erick Rodriguez <erickrod54@gmail.com>
2023-11-14 21:30:00 -0500

    Discover-videos-app - version 4.03 - Loading - Features: Building Loading component

---
+++ components/loading/loading.component.jsx
@@ -0,0 +1,17 @@
+import styles from './loading.module.css';
+
+/**Discover-videos-app - version 4.03  - Loading - 
+ * Features:
+ * 
+ *      --> Building loading Component
+ * 
+ * Note: reference to the source of the css:
+ *
+ *  source: https://projects.lukehaas.me/css-loaders/ 
+*/
+
+const Loading = () => {
+    return <p className={styles.loader}>Loading...</p>
+}
+
+export default Loading;

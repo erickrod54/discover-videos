8494191e6a7167da539ffff2d9b85ea57f674989
Erick Rodriguez <erickrod54@gmail.com>
2023-11-14 21:30:00 -0500

    Discover-videos-app - version 4.03 - components > index js - Features: Importing and exporting Loading

--- components/index.js
+++ components/index.js
@@ -1,16 +1,17 @@
 import Banner from "./banner.component";
 import Card from "./card/card.component";
 import SectionCards from "./card/section.cards.component";
+import Loading from "./loading/loading.component";
 
 import Navbar from "./nav/navbar.component";
 
 
-/**Discover-videos-app - version 1.19  - components > index js - Features:
+/**Discover-videos-app - version 4.03  - components > index js - Features:
  * 
- *      --> Updating Card import.
+ *      --> Importing and exporting 'Loading'.
  * 
  * Note: This is part of the initial composition of the app,
  * after this step every component will developed indiviually
 */
 
-export { Navbar, Banner, Card, SectionCards }
+export { Navbar, Banner, Card, SectionCards, Loading }

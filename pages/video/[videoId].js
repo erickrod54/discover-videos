b230d00d47f3d772461232153c8bee7a40c2a9bc
Erick Rodriguez <erickrod54@gmail.com>
2023-11-15 21:30:00 -0500

    Discover-videos-app - version 4.03 - [videoId].js - Features: Building dynamic route to video page

---
+++ pages/video/[videoId].js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+/**Discover-videos-app - version 4.03  - [videoId].js - 
+ * Features:
+ * 
+ *      --> Building dynamic route to video page
+ * 
+ * Note: the test is made when i try to manually set
+ * the url to the / directory
+*/
+
+
+const Video = () => {
+
+    return(
+        <h2>Video Page</h2>
+    )
+}
+
+export default Video;

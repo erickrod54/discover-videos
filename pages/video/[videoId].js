a7ca09428470b65ab1d096cb6caea6bc5ba34065
Erick Rodriguez <erickrod54@gmail.com>
2023-11-20 11:38:57 -0500

    Discover-videos-app - version 4.11 - [video] js - Fetaures: Adding youtube iframe to the modal

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -3,10 +3,10 @@ import React from "react";
 import Modal from 'react-modal';
 import styles from './video.module.css'
 
-/**Discover-videos-app - version 4.10  - [videoId].js - 
+/**Discover-videos-app - version 4.11  - [videoId].js - 
  * Features:
  * 
- *      --> Adding styles 'container' and 'overlay'
+ *      --> Adding youtube 'iframe' to the modal
  * 
  * Note: reference docs:
  * 
@@ -29,8 +29,11 @@ const Video = () => {
             onRequestClose={() => router.back()}
             overlayClassName={styles.overlay}
       >
-        <div>Modal Body</div>
-        </Modal>
+      <iframe id="ytplayer" type="text/html" width="800" height="500"
+        src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+        frameborder="0">
+      </iframe>  
+      </Modal>
         </div>
     )
 }

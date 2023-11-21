f6ecf2133a827cb0d5ee9ba63366cfbfa1e13e11
Erick Rodriguez <erickrod54@gmail.com>
2023-11-21 01:50:47 -0500

    Discover-videos-app - version 4.15 - [videoId].js - Features: Adding structure for modalBody

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -2,11 +2,12 @@ import { useRouter } from "next/router";
 import React from "react";
 import Modal from 'react-modal';
 import styles from './video.module.css'
+import clsx from 'classnames';
 
-/**Discover-videos-app - version 4.14  - [videoId].js - 
+/**Discover-videos-app - version 4.15  - [videoId].js - 
  * Features:
  * 
- *      --> Adding 'videoPlayer' styles
+ *      --> Adding structure for 'modalBody'
  * 
  * Note: reference docs:
  * 
@@ -18,8 +19,18 @@ Modal.setAppElement('#__next')
 const Video = () => {
 
     const router = useRouter();
+
+    const video = {
+        title: 'Hi cool Marvel',
+        publishTime: '1990-01-01',
+        description:'the full marvel movies, all the story developed in order to get an amazing marvel time',
+        channelTitle: 'Marvel Studios',
+        viewCount:10000    
+    }
     
     console.log({router})
+
+    const { title, publishTime, description, channelTitle, viewCount } = video
 
     return(
         <div className={styles.container}>
@@ -29,15 +40,41 @@ const Video = () => {
             onRequestClose={() => router.back()}
             overlayClassName={styles.overlay}
       >
-      <iframe 
-        id="ytplayer" 
-        className={styles.videoPlayer}
-        type="text/html" 
-        width="800" 
-        height="500"
-        src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=1&origin=http://example.com&controls=0&rel=1`}
-        frameborder="0">
-      </iframe>  
+        <iframe 
+            id="ytplayer" 
+            className={styles.videoPlayer}
+            type="text/html" 
+            width="800" 
+            height="500"
+            src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=1&origin=http://example.com&controls=0&rel=1`}
+            frameborder="0">
+        </iframe>
+
+        <div className={styles.modalBody}>
+            <div className={styles.modalBodyContent}>
+                <div className={styles.col1}>
+                    <p className={styles.publishTime}>
+                        {publishTime}
+                    </p>
+                    <p className={styles.title}>
+                        {title}
+                    </p>
+                    <p className={styles.description}>
+                        {description}
+                    </p>
+                </div>
+                <div className={styles.col2}>
+                <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                    <span className={styles.textColor}>Cast: </span>
+                    <span className={styles.channelTitle}>{channelTitle}</span>
+                </p>
+                <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                    <span className={styles.textColor}>View Count: </span>
+                    <span className={styles.channelTitle}>{viewCount}</span>
+                </p>
+                </div>
+            </div>
+        </div>  
       </Modal>
         </div>
     )

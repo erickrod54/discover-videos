3b0c524069d9ef1dc4353dd0b086c7d242325211
Erick Rodriguez <erickrod54@gmail.com>
2023-11-24 21:30:00 -0500

    Server Files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "539a9418fb37597c"; }
+/******/ 		__webpack_require__.h = function() { return "8b2d0d1e708f478b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
